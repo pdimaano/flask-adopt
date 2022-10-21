42ca8e00a1fe9a3902b26687fe2ab00f9c600058
Philip Dimaano <pdimaano7@gmail.com>
2022-10-21 12:31:34 -0700

    Created model for Pet

--- models.py
+++ models.py
@@ -14,3 +14,21 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """Model for Pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text,
+                          nullable=False,
+                          default='')
+    age = db.Column(db.Text, nullable=False)
+    notes = db.Column(db.Text, nullable=True)
+    available = db.Column(db.Boolean, default=True)
